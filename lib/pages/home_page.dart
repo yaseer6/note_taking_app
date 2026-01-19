67e0d1f3dc83b9a6104ca29ddb5cc6612c379efb
yaseer6 <yaseerintikhab@gmail.com>
2026-01-19 14:43:27 +0500

    extracted appBar to its separate function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -55,16 +55,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _buildAppBar(),
       body: SafeArea(
         child: Column(
           children: [
-            // Header Section
-            _buildHeader(),
             const Divider(
-              thickness: 1,
+              thickness: 2,
               indent: 16,
               endIndent: 16,
-              color: Colors.black12,
+              color: Color.fromRGBO(0, 0, 0, 0.08),
             ),
             const SizedBox(height: 8),
 
@@ -82,63 +81,15 @@ class _HomePageState extends State<HomePage> {
               child: IndexedStack(
                 index: _selectedTabIndex,
                 children: [
-                  FutureBuilder(
-                    future: _notesFuture,
-                    builder: (context, snapshot) {
-                      if(snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-
-                      if(snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}'),);
-                      }
-
-                      if(!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return const Center(child: Text('No notes yet'));
-                      }
-
-                      final notes = snapshot.data!;
-
-                      return NotesList(
-                        notes: notes,
-                        fromPage: AppRouter.home,
-                        onRefresh: () {
-                          _notesRefresh.value++;
-                        },
-                      );
-                    },
-                  ),
-                  FutureBuilder(
-                    future: _foldersFuture,
-                    builder: (context, snapshot) {
-                      if(snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-
-                      if(snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}'),);
-                      }
-
-                      if(!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return const Center(child: Text('No folders found'));
-                      }
-
-                      final folders = snapshot.data!;
-
-                      return FoldersList(
-                        folders: folders,
-                        onRefresh: () {
-                          _foldersRefresh.value++;
-                        },
-                      );
-                    },
-                  ),
+                  _buildNotesList(),
+                  _buildFoldersList(),
                 ],
               ),
             ),
           ],
         ),
       ),
+
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _onFabPressed,
@@ -154,38 +105,42 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildHeader() {
-    final theme = Theme.of(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-      child: Row(
-        children: [
-          const CircleAvatar(
-            backgroundImage: AssetImage('assets/images/profile_pic.jpg'),
-            radius: 25,
-          ),
-          const SizedBox(width: 16),
-          Text(
-            'Floyd Lawton',
-            style: theme.textTheme.titleMedium?.copyWith(
-              fontWeight: FontWeight.bold,
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      titleSpacing: 0,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/profile_pic.jpg'),
+              radius: 25,
             ),
-          ),
-          const Spacer(),
-          //search button
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search_rounded),
-            iconSize: 28,
-          ),
-          //menu button
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_horiz_rounded),
-            iconSize: 28,
-          ),
-        ],
+            const SizedBox(width: 16),
+            Text(
+              'Floyd Lawton',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
+      actionsPadding: EdgeInsets.fromLTRB(0, 0, 4, 0),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search_rounded),
+          iconSize: 28,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz_rounded),
+          iconSize: 28,
+        ),
+      ],
     );
   }
 
@@ -203,6 +158,63 @@ class _HomePageState extends State<HomePage> {
           fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
         ),
       ),
+    );
+  }
+
+  Widget _buildNotesList() {
+    return FutureBuilder(
+      future: _notesFuture,
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if(snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'),);
+        }
+
+        if(!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No notes yet'));
+        }
+
+        final notes = snapshot.data!;
+
+        return NotesList(
+          notes: notes,
+          fromPage: AppRouter.home,
+          onRefresh: () {
+            _notesRefresh.value++;
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildFoldersList() {
+    return FutureBuilder(
+      future: _foldersFuture,
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if(snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'),);
+        }
+
+        if(!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No folders found'));
+        }
+
+        final folders = snapshot.data!;
+
+        return FoldersList(
+          folders: folders,
+          onRefresh: () {
+            _foldersRefresh.value++;
+          },
+        );
+      },
     );
   }
 
